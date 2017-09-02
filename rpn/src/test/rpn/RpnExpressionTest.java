f1431d1f3a9e35c2649ba8b39bbffbdb1a9d1b2f
Ubu <ubald2b@gmail.com>
2017-09-03 01:21:18 +0200

    [NEW] RpnExpression should be comparable

--- rpn/src/test/rpn/RpnExpressionTest.java
+++ rpn/src/test/rpn/RpnExpressionTest.java
@@ -2,9 +2,17 @@ package rpn;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class RpnExpressionTest {
     @Test(expected = AssertionError.class)
     public void should_not_allow_to_create_with_null_expression() throws Exception {
         RpnExpression.of(null);
+    }
+
+    @Test
+    public void should_be_comparable() throws Exception {
+        assertThat(RpnExpression.of("")).isEqualTo(RpnExpression.of(""));
+        assertThat(RpnExpression.of("")).isNotEqualTo(RpnExpression.of("1"));
     }
 }
