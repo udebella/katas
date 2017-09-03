216888b7ae9805b87cb3b846762c51e7260f303e
Ubu <ubald2b@gmail.com>
2017-09-03 15:25:00 +0200

    [ENH] Following calisthenics rules : apply in tests

--- rpn/src/test/rpn/numbers/NumberMultiplyTest.java
+++ rpn/src/test/rpn/numbers/NumberMultiplyTest.java
@@ -3,25 +3,42 @@ package rpn.numbers;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static rpn.numbers.Number.ZERO;
 
 public class NumberMultiplyTest {
     @Test
     public void multiply_zero_by_zero_should_produce_zero() throws Exception {
-        assertThat(Number.ZERO.multiply(Number.ZERO)).isEqualTo(Number.ZERO);
+        final Number result = ZERO.multiply(ZERO);
+
+        assertThat(result).isEqualTo(ZERO);
     }
 
     @Test
     public void multiply_a_number_by_zero_should_produce_zero() throws Exception {
-        assertThat(Number.of("5").multiply(Number.ZERO)).isEqualTo(Number.ZERO);
+        final Number five = Number.of("5");
+
+        final Number result = five.multiply(ZERO);
+
+        assertThat(result).isEqualTo(ZERO);
     }
 
     @Test
     public void multiply_zero_by_a_number_should_produce_zero() throws Exception {
-        assertThat(Number.ZERO.multiply(Number.of("5"))).isEqualTo(Number.ZERO);
+        final Number five = Number.of("5");
+
+        final Number result = ZERO.multiply(five);
+
+        assertThat(result).isEqualTo(ZERO);
     }
 
     @Test
     public void multiply_two_numbers_should_produce_multiplication() throws Exception {
-        assertThat(Number.of("2").multiply(Number.of("5"))).isEqualTo(Number.of("10"));
+        final Number two = Number.of("2");
+        final Number five = Number.of("5");
+        final Number ten = Number.of("10");
+
+        final Number result = two.multiply(five);
+
+        assertThat(result).isEqualTo(ten);
     }
 }
