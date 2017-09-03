eae0d3366d38d83cb3c7e85bacd3778458e50b8f
Ubu <ubald2b@gmail.com>
2017-09-03 15:30:42 +0200

    [ENH] Following calisthenics rules : apply in substraction tests

--- rpn/src/test/rpn/numbers/NumberSubstractTest.java
+++ rpn/src/test/rpn/numbers/NumberSubstractTest.java
@@ -1,26 +1,45 @@
 package rpn.numbers;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static rpn.numbers.Number.ZERO;
 
 public class NumberSubstractTest {
     @Test
     public void substract_zero_to_zero_should_produce_zero() throws Exception {
-        Assertions.assertThat(Number.ZERO.substract(Number.ZERO)).isEqualTo(Number.ZERO);
+        final Number result = ZERO.substract(ZERO);
+
+        assertThat(result).isEqualTo(ZERO);
     }
 
     @Test
     public void substract_a_number_to_zero_should_produce_the_same_number() throws Exception {
-        Assertions.assertThat(Number.of("1").substract(Number.ZERO)).isEqualTo(Number.of("1"));
+        final Number one = Number.of("1");
+
+        final Number result = one.substract(ZERO);
+
+        assertThat(result).isEqualTo(one);
     }
 
     @Test
     public void substract_zero_to_a_number_should_produce_the_opposite() throws Exception {
-        Assertions.assertThat(Number.ZERO.substract(Number.of("1"))).isEqualTo(Number.of("-1"));
+        final Number one = Number.of("1");
+        final Number minusOne = Number.of("-1");
+
+        final Number result = ZERO.substract(one);
+
+        assertThat(result).isEqualTo(minusOne);
     }
 
     @Test
     public void substract_two_numbers_should_produce_substraction() throws Exception {
-        Assertions.assertThat(Number.of("1").substract(Number.of("2"))).isEqualTo(Number.of("-1"));
+        final Number one = Number.of("1");
+        final Number two = Number.of("2");
+        final Number minusOne = Number.of("-1");
+
+        final Number result = one.substract(two);
+
+        assertThat(result).isEqualTo(minusOne);
     }
 }
