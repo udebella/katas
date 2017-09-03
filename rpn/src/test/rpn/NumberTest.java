3a405b072611ef1320760e722f450718eceee518
Ubu <ubald2b@gmail.com>
2017-09-03 14:39:45 +0200

    [ENH] Verify that number are comparable

--- rpn/src/test/rpn/NumberTest.java
+++ rpn/src/test/rpn/NumberTest.java
@@ -11,6 +11,12 @@ public class NumberTest {
     }
 
     @Test
+    public void numbers_should_be_comparable() throws Exception {
+        assertThat(Number.of("1").equals(Number.of("1"))).isTrue();
+        assertThat(Number.of("1").equals(Number.of("2"))).isFalse();
+    }
+
+    @Test
     public void creating_numbers_with_empty_string_should_produce_zero() throws Exception {
         assertThat(Number.of("")).isEqualTo(Number.ZERO);
     }
