c99ef16ce53e3eae3a3bf7a595fe061e289876c5
Ubu <ubald2b@gmail.com>
2017-09-03 16:08:42 +0200

    [ENH] Following calisthenics rules : one dot per line in RpnExpressionFeature

--- rpn/src/test/rpn/RpnExpressionFeature.java
+++ rpn/src/test/rpn/RpnExpressionFeature.java
@@ -9,13 +9,17 @@ public class RpnExpressionFeature {
     public void multiple_operations_feature() throws Exception {
         final RpnExpression expression = RpnExpression.of("3 5 8 * 7 + *");
 
-        assertThat(expression).isEqualTo(RpnExpression.of("141"));
+        final RpnExpression result = RpnExpression.of("141");
+
+        assertThat(expression).isEqualTo(result);
     }
 
     @Test
     public void multiple_results() throws Exception {
         final RpnExpression expression = RpnExpression.of("7 2 - 3 4");
 
-        assertThat(expression).isEqualTo(RpnExpression.of("5 3 4"));
+        final RpnExpression result = RpnExpression.of("5 3 4");
+
+        assertThat(expression).isEqualTo(result);
     }
 }
