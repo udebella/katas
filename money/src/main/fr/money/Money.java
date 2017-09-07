2e89f039d9d4b466e78a41b404f82aae1906eefd
Ubald de Bellabre <ubald.de-bellabre@lcdlv.fr>
2017-09-07 17:33:22 +0200

    [NEW] Create money object from amount and currency

--- money/src/main/fr/money/Money.java
+++ money/src/main/fr/money/Money.java
@@ -1,9 +1,6 @@
 package fr.money;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 public class Money {
     public Money(Amount amount, Currency dinar) {
-        throw new NotImplementedException();
     }
 }
