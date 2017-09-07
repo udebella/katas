10ef4a72a81e63b167c00f80e1b3739424fd6779
Ubald de Bellabre <ubald.de-bellabre@lcdlv.fr>
2017-09-07 17:33:57 +0200

    [ENH] Improve money test readability

--- money/src/test/fr/money/MoneyTest.java
+++ money/src/test/fr/money/MoneyTest.java
@@ -1,12 +1,16 @@
 package fr.money;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MoneyTest {
 
     @Test
     public void should_allow_to_create_money_from_amount_and_currency() throws Exception {
-        Assertions.assertThat(new Money(new Amount(0), Currency.EUROS)).isNotNull();
+        final Amount amount = new Amount(0);
+        final Money money = new Money(amount, Currency.EUROS);
+
+        assertThat(money).isNotNull();
     }
 }
