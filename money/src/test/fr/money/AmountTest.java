aa4497a8159ac963b30e8e5b758b2b9745db1d3f
Ubald de Bellabre <ubald.de-bellabre@lcdlv.fr>
2017-09-07 10:26:47 +0200

    [ENH] Improve amount test readability

--- money/src/test/fr/money/AmountTest.java
+++ money/src/test/fr/money/AmountTest.java
@@ -1,11 +1,14 @@
 package fr.money;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AmountTest {
     @Test
     public void should_allow_to_create_amount_from_value() throws Exception {
-        Assertions.assertThat(new Amount(0)).isNotNull();
+        final Amount amount = new Amount(0);
+
+        assertThat(amount).isNotNull();
     }
 }
