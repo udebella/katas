9857eee37f01cf529dfb4b1ca306c0910c63c840
Ubald de Bellabre <ubald.de-bellabre@lcdlv.fr>
2017-09-08 14:48:12 +0200

    [ENH] Extract variable for readability

--- money/src/test/fr/money/CurrencyTest.java
+++ money/src/test/fr/money/CurrencyTest.java
@@ -1,18 +1,21 @@
 package fr.money;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 import java.math.BigDecimal;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CurrencyTest {
     @Test
     public void should_allow_to_convert_euros_from_euros() throws Exception {
-        Assertions.assertThat(Currency.EUROS.to(Currency.EUROS)).isEqualTo(BigDecimal.ONE);
+        assertThat(Currency.EUROS.to(Currency.EUROS)).isEqualTo(BigDecimal.ONE);
     }
 
     @Test
     public void should_allow_to_convert_from_euros_to_dollars() throws Exception {
-        Assertions.assertThat(Currency.EUROS.to(Currency.DOLLARS)).isEqualTo(new BigDecimal(84).divide(new BigDecimal(100)));
+        final BigDecimal eurosToDollars = new BigDecimal(84).divide(new BigDecimal(100));
+
+        assertThat(Currency.EUROS.to(Currency.DOLLARS)).isEqualTo(eurosToDollars);
     }
 }
