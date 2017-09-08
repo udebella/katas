fd5213059f1ec04d1db7a51bfefb4ce161a3ed3c
Ubald de Bellabre <ubald.de-bellabre@lcdlv.fr>
2017-09-08 15:05:58 +0200

    [ENH] Checking conversion rate from euros to other currencies

--- money/src/main/fr/money/Currency.java
+++ money/src/main/fr/money/Currency.java
@@ -4,14 +4,14 @@ import java.math.BigDecimal;
 
 public enum Currency {
     WONS(new BigDecimal(75).divide(new BigDecimal(100_000))),
-    FORINTS(new BigDecimal(33).divide(new BigDecimal(1_000))),
-    PENCES(new BigDecimal(109).divide(new BigDecimal(100_000))),
-    HONG_KONG_DOLLARS(new BigDecimal(84).divide(new BigDecimal(100))),
-    CZECK_KORUNAS(new BigDecimal(3831).divide(new BigDecimal(10_000))),
+    FORINTS(new BigDecimal(33).divide(new BigDecimal(10_000))),
+    PENCES(new BigDecimal(109).divide(new BigDecimal(10_000))),
+    HONG_KONG_DOLLARS(new BigDecimal(11).divide(new BigDecimal(100))),
+    CZECK_KORUNAS(new BigDecimal(3831).divide(new BigDecimal(100_000))),
     DOLLARS(new BigDecimal(84).divide(new BigDecimal(100))),
-    YENS(new BigDecimal(76).divide(new BigDecimal(1_000))),
+    YENS(new BigDecimal(76).divide(new BigDecimal(1_0000))),
     EUROS(BigDecimal.ONE),
-    DINAR(new BigDecimal(72).divide(new BigDecimal(10_000)));
+    DINAR(new BigDecimal(72).divide(new BigDecimal(100_000)));
 
     private final BigDecimal rate;
 
--- money/src/test/fr/money/CurrencyTest.java
+++ money/src/test/fr/money/CurrencyTest.java
@@ -4,18 +4,25 @@ import org.junit.Test;
 
 import java.math.BigDecimal;
 
+import static fr.money.Currency.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CurrencyTest {
     @Test
     public void should_allow_to_convert_euros_from_euros() throws Exception {
-        assertThat(Currency.EUROS.to(Currency.EUROS)).isEqualTo(BigDecimal.ONE);
+        assertThat(EUROS.to(EUROS)).isEqualTo(BigDecimal.ONE);
     }
 
     @Test
-    public void should_allow_to_convert_from_euros_to_dollars() throws Exception {
-        final BigDecimal eurosToDollars = new BigDecimal(84).divide(new BigDecimal(100));
-
-        assertThat(Currency.EUROS.to(Currency.DOLLARS)).isEqualTo(eurosToDollars);
+    public void conversion_rate_from_euros_to_other_currencies_should_be_right() throws Exception {
+        assertThat(EUROS.to(DINAR).toString()).isEqualTo("0.00072");
+        assertThat(EUROS.to(WONS).toString()).isEqualTo("0.00075");
+        assertThat(EUROS.to(FORINTS).toString()).isEqualTo("0.0033");
+        assertThat(EUROS.to(PENCES).toString()).isEqualTo("0.0109");
+        assertThat(EUROS.to(HONG_KONG_DOLLARS).toString()).isEqualTo("0.11");
+        assertThat(EUROS.to(CZECK_KORUNAS).toString()).isEqualTo("0.03831");
+        assertThat(EUROS.to(DOLLARS).toString()).isEqualTo("0.84");
+        assertThat(EUROS.to(YENS).toString()).isEqualTo("0.0076");
+        assertThat(EUROS.to(EUROS).toString()).isEqualTo("1");
     }
 }
