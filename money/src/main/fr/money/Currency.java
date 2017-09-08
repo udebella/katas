a141de040bd87e1505cc796abce082e64e1615bc
Ubald de Bellabre <ubald.de-bellabre@lcdlv.fr>
2017-09-08 14:47:04 +0200

    [NEW] Conversion rate from euros to another currency

--- money/src/main/fr/money/Currency.java
+++ money/src/main/fr/money/Currency.java
@@ -3,9 +3,23 @@ package fr.money;
 import java.math.BigDecimal;
 
 public enum Currency {
-    WONS, FORINTS, PENCES, HONG_KONG_DOLLARS, CZECK_KORUNAS, DOLLARS, YENS, EUROS, DINAR;
+    WONS(new BigDecimal(75).divide(new BigDecimal(100_000))),
+    FORINTS(new BigDecimal(33).divide(new BigDecimal(1_000))),
+    PENCES(new BigDecimal(109).divide(new BigDecimal(100_000))),
+    HONG_KONG_DOLLARS(new BigDecimal(84).divide(new BigDecimal(100))),
+    CZECK_KORUNAS(new BigDecimal(3831).divide(new BigDecimal(10_000))),
+    DOLLARS(new BigDecimal(84).divide(new BigDecimal(100))),
+    YENS(new BigDecimal(76).divide(new BigDecimal(1_000))),
+    EUROS(BigDecimal.ONE),
+    DINAR(new BigDecimal(72).divide(new BigDecimal(10_000)));
+
+    private final BigDecimal rate;
+
+    Currency(BigDecimal rate) {
+        this.rate = rate;
+    }
 
     public BigDecimal to(Currency currency) {
-        return BigDecimal.ONE;
+        return currency.rate;
     }
 }
--- money/src/test/fr/money/CurrencyTest.java
+++ money/src/test/fr/money/CurrencyTest.java
@@ -10,4 +10,9 @@ public class CurrencyTest {
     public void should_allow_to_convert_euros_from_euros() throws Exception {
         Assertions.assertThat(Currency.EUROS.to(Currency.EUROS)).isEqualTo(BigDecimal.ONE);
     }
+
+    @Test
+    public void should_allow_to_convert_from_euros_to_dollars() throws Exception {
+        Assertions.assertThat(Currency.EUROS.to(Currency.DOLLARS)).isEqualTo(new BigDecimal(84).divide(new BigDecimal(100)));
+    }
 }
