22e019a5d9406beae89e671d1c25125da7024c35
Ubald de Bellabre <ubald.de-bellabre@lcdlv.fr>
2017-09-11 15:56:41 +0200

    [ENH] Import static for readability

--- money/src/test/fr/money/WalletTest.java
+++ money/src/test/fr/money/WalletTest.java
@@ -3,6 +3,7 @@ package fr.money;
 import org.junit.Before;
 import org.junit.Test;
 
+import static fr.money.Currency.EUROS;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WalletTest {
@@ -16,27 +17,27 @@ public class WalletTest {
     @Test
     public void wallet_should_be_empty_by_default() throws Exception {
         final Amount expectedAmount = new Amount(0);
-        final Money expectedMoney = new Money(expectedAmount, Currency.EUROS);
-        assertThat(wallet.valueIn(Currency.EUROS)).isEqualTo(expectedMoney);
+        final Money expectedMoney = new Money(expectedAmount, EUROS);
+        assertThat(wallet.valueIn(EUROS)).isEqualTo(expectedMoney);
     }
 
     @Test
     public void putting_zero_amount_in_the_wallet_should_not_update_the_value() throws Exception {
         final Amount zeroAmount = new Amount(0);
-        final Money zeroMoney = new Money(zeroAmount, Currency.EUROS);
+        final Money zeroMoney = new Money(zeroAmount, EUROS);
 
         wallet.put(zeroMoney);
 
-        assertThat(wallet.valueIn(Currency.EUROS)).isEqualTo(zeroMoney);
+        assertThat(wallet.valueIn(EUROS)).isEqualTo(zeroMoney);
     }
 
     @Test
     public void putting_ten_euros_in_the_wallet_should_update_the_value() throws Exception {
         final Amount ten = new Amount(10);
-        final Money tenEuros = new Money(ten, Currency.EUROS);
+        final Money tenEuros = new Money(ten, EUROS);
 
         wallet.put(tenEuros);
 
-        assertThat(wallet.valueIn(Currency.EUROS)).isEqualTo(tenEuros);
+        assertThat(wallet.valueIn(EUROS)).isEqualTo(tenEuros);
     }
 }
