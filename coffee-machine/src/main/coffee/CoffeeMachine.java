a3cf99e9e8ab6c48a46ebac7e2b71ea5cb1ea74f
Ubald de Bellabre <ubald.de-bellabre@lcdlv.fr>
2017-09-13 11:53:38 +0200

    [ENH] Collapse two if to handle sugar

--- coffee-machine/src/main/coffee/CoffeeMachine.java
+++ coffee-machine/src/main/coffee/CoffeeMachine.java
@@ -4,11 +4,9 @@ public class CoffeeMachine {
     public String handle(CustomerCommand customerCustomer) {
         return DrinkType.getCommandFromName(customerCustomer.getDrinkType())
                 .map(command -> {
-                    if (customerCustomer.getSugarNumber() == 2) {
-                        return command + ":2:0";
-                    }
-                    if (customerCustomer.getSugarNumber() == 1) {
-                        return command + ":1:0";
+                    final int sugarNumber = customerCustomer.getSugarNumber();
+                    if (sugarNumber > 0) {
+                        return command + ":" + sugarNumber + ":0";
                     }
                     return command + "::";
                 }).get();
