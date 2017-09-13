f75d64801e7b719778a8ace90000e45490aee5d5
Ubald de Bellabre <ubald.de-bellabre@lcdlv.fr>
2017-09-13 15:34:35 +0200

    [ENH] Do not expose enum internal representation

--- coffee-machine/src/main/coffee/CoffeeMachine.java
+++ coffee-machine/src/main/coffee/CoffeeMachine.java
@@ -1,22 +1,14 @@
 package coffee;
 
 import java.util.NoSuchElementException;
-import java.util.stream.Stream;
 
 public class CoffeeMachine {
 
     public String handle(CustomerCommand customerCommand) {
-        final DrinkType drinkType = Stream.of(DrinkType.values())
-                .filter(dt -> dt.getName().equals(customerCommand.getDrinkType()))
-                .findAny()
+        return DrinkType
+                .getDrinkType(customerCommand.getDrinkType())
+                .map(drinkType -> drinkType.formatMessage(customerCommand))
                 .orElseThrow(NoSuchElementException::new);
-        if (drinkType.getPrice() <= customerCommand.getMoney()) {
-            final int sugarNumber = customerCommand.getSugarNumber();
-            if (sugarNumber > 0) {
-                return drinkType.getCommand() + ":" + sugarNumber + ":0";
-            }
-            return drinkType.getCommand() + "::";
-        }
-        return "M:Not enough money : 50 is missing";
     }
+
 }
--- coffee-machine/src/main/coffee/DrinkType.java
+++ coffee-machine/src/main/coffee/DrinkType.java
@@ -1,9 +1,12 @@
 package coffee;
 
+import java.util.Optional;
+import java.util.stream.Stream;
+
 public enum DrinkType {
-    COFFEE("Coffee", "C" , 60),
-    CHOCOLATE("Chocolate", "H" ,50),
-    TEA("Tea", "T" ,40);
+    COFFEE("Coffee", "C", 60),
+    CHOCOLATE("Chocolate", "H", 50),
+    TEA("Tea", "T", 40);
 
     private final String name;
     private final String command;
@@ -15,15 +18,21 @@ public enum DrinkType {
         this.price = price;
     }
 
-    public  int getPrice() {
-        return price;
+    static Optional<DrinkType> getDrinkType(String drinkType) {
+        return Stream.of(values())
+                .filter(dt -> dt.name.equals(drinkType))
+                .findAny();
     }
 
-    public String getCommand() {
-        return command;
+    public String formatMessage(CustomerCommand customerCommand) {
+        if (this.price <= customerCommand.getMoney()) {
+            final int sugarNumber = customerCommand.getSugarNumber();
+            if (sugarNumber > 0) {
+                return this.command + ":" + sugarNumber + ":0";
+            }
+            return this.command + "::";
+        }
+        return "M:Not enough money : 50 is missing";
     }
 
-    public String getName() {
-        return name;
-    }
 }
