2a1dac99580c56b9da36668f0534d239d48ed171
Ubald de Bellabre <ubald.de-bellabre@lcdlv.fr>
2017-09-15 11:39:53 +0200

    [ENH] Tennis game 2 : Extract method format score

--- tennis/src/main/java/tennis/game2/TennisGame2.java
+++ tennis/src/main/java/tennis/game2/TennisGame2.java
@@ -2,11 +2,10 @@ package tennis.game2;
 
 import tennis.TennisGame;
 
-public class TennisGame2 implements TennisGame
-{
+public class TennisGame2 implements TennisGame {
     public int P1point = 0;
     public int P2point = 0;
-    
+
     public String P1res = "";
     public String P2res = "";
     private String player1Name;
@@ -17,74 +16,69 @@ public class TennisGame2 implements TennisGame
         this.player2Name = player2Name;
     }
 
-    public String getScore(){
+    public String getScore() {
         String score = "";
-        if (P1point==0) {
-            P1res = "Love";
-        }
-        if (P1point==1) {
-            P1res = "Fifteen";
-        }
-        if (P1point==2) {
-            P1res = "Thirty";
-        }
-        if (P1point==3) {
-            P1res = "Forty";
-        }
-        if (P2point==0) {
+        P1res = formatScore(P1point);
+        if (P2point == 0) {
             P2res = "Love";
         }
-        if (P2point==1) {
+        if (P2point == 1) {
             P2res = "Fifteen";
         }
-        if (P2point==2) {
+        if (P2point == 2) {
             P2res = "Thirty";
         }
-        if (P2point==3) {
+        if (P2point == 3) {
             P2res = "Forty";
         }
 
-        if (P1point>P2point && P1point < 4)
-        {
+        if (P1point > P2point && P1point < 4) {
             score = P1res + "-" + P2res;
         }
-        if (P2point>P1point && P2point < 4)
-        {
+        if (P2point > P1point && P2point < 4) {
             score = P1res + "-" + P2res;
         }
-        if (P1point == P2point && P1point < 4)
-        {
+        if (P1point == P2point && P1point < 4) {
             score = P1res + "-All";
         }
-        if (P1point==P2point && P1point>=3)
+        if (P1point == P2point && P1point >= 3)
             score = "Deuce";
-        
-        if (P1point > P2point && P2point >= 3)
-        {
+
+        if (P1point > P2point && P2point >= 3) {
             score = "Advantage player1";
         }
-        
-        if (P2point > P1point && P1point >= 3)
-        {
+
+        if (P2point > P1point && P1point >= 3) {
             score = "Advantage player2";
         }
-        
-        if (P1point>=4 && (P1point-P2point)>=2)
-        {
+
+        if (P1point >= 4 && (P1point - P2point) >= 2) {
             score = "Win for player1";
         }
-        if (P2point>=4 && (P2point-P1point)>=2)
-        {
+        if (P2point >= 4 && (P2point - P1point) >= 2) {
             score = "Win for player2";
         }
         return score;
     }
 
-    public void incrementFirstPlayerScore(){
+    private String formatScore(int points) {
+        switch (points) {
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            default:
+                return "Forty";
+        }
+    }
+
+    public void incrementFirstPlayerScore() {
         P1point++;
     }
-    
-    public void incrementSecondPlayerScore(){
+
+    public void incrementSecondPlayerScore() {
         P2point++;
     }
 
