d78d9301e9ac2d74a9a03c2476db8819b1d3ea55
Ubald de Bellabre <ubald.de-bellabre@lcdlv.fr>
2017-09-15 10:37:55 +0200

    [ENH] Tennis game 2 : Using equals to compare string

--- tennis/src/main/java/tennis/TennisGame2.java
+++ tennis/src/main/java/tennis/TennisGame2.java
@@ -128,7 +128,7 @@ public class TennisGame2 implements TennisGame
     }
 
     public void wonPoint(String player) {
-        if (player == "player1")
+        if ("player1".equals(player))
             P1Score();
         else
             P2Score();
