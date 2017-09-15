baa228e84a0e0145d89ce44267afd2cfd0fcd989
Ubald de Bellabre <ubald.de-bellabre@lcdlv.fr>
2017-09-15 16:55:14 +0200

    [ENH] Tennis game 2 : Clarifying winning case

--- tennis/src/main/java/tennis/game2/TennisGame2.java
+++ tennis/src/main/java/tennis/game2/TennisGame2.java
@@ -3,6 +3,7 @@ package tennis.game2;
 import tennis.TennisGame;
 
 public class TennisGame2 implements TennisGame {
+    public static final String SCORE_SEPARATOR = "-";
     private int P1point = 0;
     private int P2point = 0;
 
@@ -16,39 +17,24 @@ public class TennisGame2 implements TennisGame {
 
     public String getScore() {
         String score = "";
+        final String firstPlayerScore = pointToScoreName(P1point);
         if (P1point < 4 || P2point < 4) {
-            score = formatEarlyGameScore();
+            score = firstPlayerScore + SCORE_SEPARATOR;
+            score += pointToScoreName(P2point);
         }
         if (P1point >= 3 && P2point >= 3) {
             score = "Advantage " + winner();
-            if (P1point == P2point) {
-                score = "Deuce";
-            }
         }
-        if (P1point >= 4 || P2point >= 4) {
-            if (Math.abs(P1point - P2point) >= 2) {
-                score = "Win for " + winner();
-            }
+        if (isWin()) {
+            score = "Win for " + winner();
         }
-        return score;
-    }
-
-    private String formatEarlyGameScore() {
-        String score;
-        String p1res = pointToScoreName(P1point);
-        String p2res = pointToScoreName(P2point);
-        score = p1res + "-" + p2res;
         if (P1point == P2point) {
-            score = p1res + "-All";
+            score = "Deuce";
+            if (P1point < 3) {
+                score = firstPlayerScore + SCORE_SEPARATOR + "All";
+            }
         }
         return score;
-    }
-
-    private String winner() {
-        if (P1point > P2point) {
-            return player1Name;
-        }
-        return player2Name;
     }
 
     private String pointToScoreName(int points) {
@@ -62,6 +48,25 @@ public class TennisGame2 implements TennisGame {
             default:
                 return "Forty";
         }
+    }
+
+    private String winner() {
+        if (P1point > P2point) {
+            return player1Name;
+        }
+        return player2Name;
+    }
+
+    private boolean isWin() {
+        return isPotentialWin() && atLeastTwoPointDifference();
+    }
+
+    private boolean isPotentialWin() {
+        return P1point > 3 || P2point > 3;
+    }
+
+    private boolean atLeastTwoPointDifference() {
+        return Math.abs(P1point - P2point) >= 2;
     }
 
     public void wonPoint(String player) {
