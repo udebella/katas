18edcec069bec16224ace5789e2c07761609d9a4
Ubu <ubald2b@gmail.com>
2019-10-04 01:22:52 +0200

    Track drinks

--- src/main/java/kata/coffee/machine/Repository.java
+++ src/main/java/kata/coffee/machine/Repository.java
@@ -4,18 +4,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Repository {
-    private final Map<Drink, Integer> soldDrinks = new HashMap<>();
+    private final Map<Drinks, Integer> soldDrinks = new HashMap<>();
 
     public void track(Order order) {
         order.register(this);
     }
 
-    public void track(Drink drink) {
+    public void track(Drinks drink) {
         soldDrinks.put(drink, soldDrinks.getOrDefault(drink, 0) + 1);
     }
 
     public void printReporting(Console console) {
-        for (Map.Entry<Drink, Integer> entry : soldDrinks.entrySet()) {
+        for (Map.Entry<Drinks, Integer> entry : soldDrinks.entrySet()) {
             console.print(entry.getKey() + " " + entry.getValue());
         }
     }
