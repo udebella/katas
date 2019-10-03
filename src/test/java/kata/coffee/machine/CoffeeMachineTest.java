bdcb460c8d20b93e8e1f3893db065506d2f864fe
Ubu <ubald2b@gmail.com>
2019-10-03 23:13:09 +0200

    Use parametrized tests

--- src/test/java/kata/coffee/machine/CoffeeMachineTest.java
+++ src/test/java/kata/coffee/machine/CoffeeMachineTest.java
@@ -1,41 +1,24 @@
 package kata.coffee.machine;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+@RunWith(JUnitParamsRunner.class)
 public class CoffeeMachineTest {
     @Test
-    public void should_send_message_to_drink_maker_for_a_tea_with_one_sugar() {
+    @Parameters({"TEA, T:1:0", "COFFEE, C:1:0", "CHOCOLATE, H:1:0", })
+    public void should_send_message_to_drink_maker(Drink drink, String command) {
         final DrinkMaker drinkMaker = mock(DrinkMaker.class);
         final CoffeeMachine coffeeMachine = new CoffeeMachine(drinkMaker);
-        final Order order = Order.of(Drink.TEA, 1);
+        final Order order = Order.of(drink, 1);
 
         coffeeMachine.make(order);
 
-        verify(drinkMaker).process("T:1:0");
-    }
-
-    @Test
-    public void should_send_message_to_drink_maker_for_a_coffee_with_one_sugar() {
-        final DrinkMaker drinkMaker = mock(DrinkMaker.class);
-        final CoffeeMachine coffeeMachine = new CoffeeMachine(drinkMaker);
-        final Order order = Order.of(Drink.COFFEE, 1);
-
-        coffeeMachine.make(order);
-
-        verify(drinkMaker).process("C:1:0");
-    }
-
-    @Test
-    public void should_send_message_to_drink_maker_for_a_chocolate_with_one_sugar() {
-        final DrinkMaker drinkMaker = mock(DrinkMaker.class);
-        final CoffeeMachine coffeeMachine = new CoffeeMachine(drinkMaker);
-        final Order order = Order.of(Drink.CHOCOLATE, 1);
-
-        coffeeMachine.make(order);
-
-        verify(drinkMaker).process("H:1:0");
+        verify(drinkMaker).process(command);
     }
 }
