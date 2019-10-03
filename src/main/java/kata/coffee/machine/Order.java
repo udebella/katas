49a729a52622e7e51516fb5e29f2e2dee5daf51c
Ubu <ubald2b@gmail.com>
2019-10-04 00:49:23 +0200

    Move OrderBuilder outside Order class

--- src/main/java/kata/coffee/machine/Order.java
+++ src/main/java/kata/coffee/machine/Order.java
@@ -4,7 +4,7 @@ public class Order {
     private final Drink drink;
     private final Amount amount;
 
-    private Order(Drink drink, Amount amount) {
+    public Order(Drink drink, Amount amount) {
         this.drink = drink;
         this.amount = amount;
     }
@@ -19,41 +19,4 @@ public class Order {
         return new OrderBuilder();
     }
 
-    public static final class OrderBuilder {
-        private int sugarNumber = 0;
-        private boolean isExtraHot = false;
-        private Drinks drink;
-        private Amount amount = Amount.of(0);
-
-        private OrderBuilder() {
-        }
-
-        public OrderBuilder withDrink(Drinks drink) {
-            this.drink = drink;
-            return this;
-        }
-
-        public OrderBuilder extraHot() {
-            this.isExtraHot = true;
-            return this;
-        }
-
-        public OrderBuilder withSugar(int sugarNumber) {
-            this.sugarNumber = sugarNumber;
-            return this;
-        }
-
-        public OrderBuilder withAmount(int amount) {
-            this.amount = Amount.of(amount);
-            return this;
-        }
-
-        public Order build() {
-            Drink d = drink;
-            if (isExtraHot) {
-                d = new ExtraHot(d);
-            }
-            return new Order(new SugarDrink(d, sugarNumber), amount);
-        }
-    }
 }
---
+++ src/main/java/kata/coffee/machine/OrderBuilder.java
@@ -0,0 +1,39 @@
+package kata.coffee.machine;
+
+public final class OrderBuilder {
+    private int sugarNumber = 0;
+    private boolean isExtraHot = false;
+    private Drinks drink;
+    private Amount amount = Amount.of(0);
+
+    OrderBuilder() {
+    }
+
+    public OrderBuilder withDrink(Drinks drink) {
+        this.drink = drink;
+        return this;
+    }
+
+    public OrderBuilder extraHot() {
+        this.isExtraHot = true;
+        return this;
+    }
+
+    public OrderBuilder withSugar(int sugarNumber) {
+        this.sugarNumber = sugarNumber;
+        return this;
+    }
+
+    public OrderBuilder withAmount(int amount) {
+        this.amount = Amount.of(amount);
+        return this;
+    }
+
+    public Order build() {
+        Drink d = drink;
+        if (isExtraHot) {
+            d = new ExtraHot(d);
+        }
+        return new Order(new SugarDrink(d, sugarNumber), amount);
+    }
+}
