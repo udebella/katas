5bacbdcae9592b99e814d53dd5eaebaf2d5198d6
Ubu <ubald2b@gmail.com>
2019-11-10 17:02:57 +0100

    Use collections instead of List in Hotel class

--- corporate-hotel-booking/src/main/java/kata/corporate/hotel/booking/Hotel.java
+++ corporate-hotel-booking/src/main/java/kata/corporate/hotel/booking/Hotel.java
@@ -1,24 +1,26 @@
 package kata.corporate.hotel.booking;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class Hotel {
-    private final List<Room> roomList;
+    private final Collection<Room> roomList;
 
-    private Hotel(List<Room> roomList) {
+    private Hotel(Collection<Room> roomList) {
         this.roomList = roomList;
     }
 
-    public static Hotel of(List<Room> roomList) {
+    public static Hotel of(Collection<Room> roomList) {
         Objects.requireNonNull(roomList, "Room list is mandatory");
         return new Hotel(roomList);
     }
 
     public Hotel addRoom(String roomType) {
-        final ArrayList<Room> newRoomList = new ArrayList<>(roomList);
-        newRoomList.add(Room.of(roomType));
+        final Room addedRoom = Room.of(roomType);
+        final Collection<Room> newRoomList = Stream.concat(roomList.stream(), Stream.of(addedRoom))
+                .collect(Collectors.toList());
         return of(newRoomList);
     }
 
