5684a12d9cc12f2fdd872f8f691b60a7c546578b
Ubu <ubald2b@gmail.com>
2019-11-19 00:49:45 +0100

    Refactoring to remove duplication in hotelServiceImpl

--- corporate-hotel-booking/src/main/java/kata/corporate/hotel/booking/HotelServiceImpl.java
+++ corporate-hotel-booking/src/main/java/kata/corporate/hotel/booking/HotelServiceImpl.java
@@ -11,7 +11,7 @@ public class HotelServiceImpl implements HotelService {
 
     @Override
     public void setRoomType(String hotelId, String roomType, int quantity) {
-        Hotel hotel = repository.find(hotelId).orElse(Hotel.of(hotelId, Collections.emptyList()));
+        Hotel hotel = findHotelBy(hotelId);
         for (int i = 0; i < quantity; i++) {
             hotel = hotel.addRoom(Room.of(roomType));
         }
