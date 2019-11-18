80417f0b8e8c77d7459cac5fbab64f88b771e93f
Ubu <ubald2b@gmail.com>
2019-11-19 00:42:52 +0100

    Make proper implementation for InMemoryHotelRepository

--- corporate-hotel-booking/src/test/java/kata/corporate/hotel/booking/InMemoryHotelRepository.java
+++ corporate-hotel-booking/src/test/java/kata/corporate/hotel/booking/InMemoryHotelRepository.java
@@ -1,15 +1,21 @@
 package kata.corporate.hotel.booking;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 public class InMemoryHotelRepository implements HotelRepository {
+    private final Set<Hotel> hotels = new HashSet<>();
+
     @Override
     public void save(Hotel hotel) {
-
+        hotels.add(hotel);
     }
 
     @Override
     public Optional<Hotel> find(String hotelId) {
-        return null;
+        return hotels.stream()
+                .filter(hotel -> hotel.matches(hotelId))
+                .findAny();
     }
 }
